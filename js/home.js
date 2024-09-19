57d8f70e1b2fcd28e50acb1ac6b165f2049ff9ce
Hemel <reaz.hemel@gmail.com>
2024-09-20 00:39:36 +0600

    updated pin and amount

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log('Home Page Added')
+console.log('Home Page Added');
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+  event.preventDefault(); //it is for preventing the page reload
+ const addMoneyInput = document.getElementById('input-add-money').value
+ console.log(addMoneyInput); 
+
+ const pinNumberInput = document.getElementById('input-pin-number').value
+ console.log(pinNumberInput);
+})
