91f96af5b3246382764b731aa0ea1a8fff2da7aa
Hemel <reaz.hemel@gmail.com>
2024-09-20 11:33:12 +0600

    updated the balance

--- js/home.js
+++ js/home.js
@@ -6,4 +6,18 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
  const pinNumberInput = document.getElementById('input-pin-number').value
  console.log(pinNumberInput);
-})
+ if(pinNumberInput === '2580'){
+  console.log('Adding Money to your account');
+const balance = document.getElementById('account-balance').innerText;
+ console.log(balance);
+
+ const addMoneyNumber = parseFloat(addMoneyInput);
+ const balanceNumber = parseFloat(balance);
+ const newBalance = addMoneyNumber + balanceNumber
+document.getElementById('account-balance').innerText = newBalance;
+ console.log(newBalance);
+ }
+ else{
+  alert('Failed to add money!Please try again')
+ }
+});
